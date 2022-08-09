20c41e3b1913ecc8ae836ebc6a7c756cb582ae3b
Kaueletta <107151699+KaueLetta@users.noreply.github.com>
2022-08-09 20:33:06 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -34,19 +34,7 @@ function checkInputs() {
     setErrorFor(password, "A senha é obrigatória.");
   } else if (passwordValue.length < 7) {
     
-    
-  <!-- Google Tag Manager -->
-    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-PX7X257');</script>
-        <!-- End Google Tag Manager -->
-      
-    <!-- Google Tag Manager (noscript) -->
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PX7X257"
-    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-    <!-- End Google Tag Manager (noscript) -->
+ 
     setErrorFor(password, "A senha precisa ter no mínimo 7 caracteres.");
   } else {
     setSuccessFor(password);
@@ -94,3 +82,25 @@ function checkEmail(email) {
     email
   );
 }
+
+   
+    window.dataLayer = window.dataLayer || [];
+    dataLayer.push({
+    'name_prod' : 'Mouse Exemplo',
+    'conversionValue' : 180.77,
+    'event' : 'Finalizacao'
+    });
+
+   
+  <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-PX7X257');</script>
+        <!-- End Google Tag Manager -->
+      
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PX7X257"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
